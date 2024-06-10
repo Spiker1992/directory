fac5c9c39d873f0ce843e15dc2e053dfdd857ed9
Vladislav Ogir <v.v.ogir@outlook.com>
2024-06-10 08:35:34 +0100

    test: cleaned up the test

--- listing_management/tests/test_create_listing.py
+++ listing_management/tests/test_create_listing.py
@@ -1,28 +1,31 @@
-from dataclasses import dataclass
+
+from dataclasses import asdict
 from django.test import TestCase
+from listing_management.commands.create_listing import CreateListing, create_listing
 from listing_management.signals import listing_created 
-from django.dispatch import receiver
 import uuid
+from listing_management.tests.utils import catch_signal
 import pytest
-import time
+from unittest import mock
 from listing_management.event_stores.listing import ListingEventStore
 
-@dataclass
-class CreateListing:
-    name: str 
-    description: str
 
 @pytest.mark.django_db
 class TestCreateListing(TestCase):
     databases = {"event_store"}
+
     def test_create_listing(self):
         payload = CreateListing("name", "description")
         stream_id = uuid.uuid4()
 
-        @receiver(listing_created, sender=ListingEventStore)
-        def asser_signal_triggered(sender, **kwargs): 
-            assert True
-
-        create_listing(stream_id, payload)
+        with catch_signal(listing_created) as signal:
+            create_listing(stream_id, payload)  
 
         event = ListingEventStore.objects.get(stream_id=stream_id)
+
+        assert event.event_payload == asdict(payload)
+        signal.assert_called_once_with(
+            signal=mock.ANY,  
+            sender=ListingEventStore,  
+            instance=event
+        )
