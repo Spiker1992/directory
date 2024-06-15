f301f3cdb204a13560c5321a717a4f732e26635e
Vladislav Ogir <v.v.ogir@outlook.com>
2024-06-16 00:07:06 +0100

    feat: added a way to mock signals

--- listing_management/signals.py
+++ listing_management/signals.py
@@ -1,3 +1,19 @@
-import django.dispatch
+from unittest.mock import Mock
+from django.dispatch import Signal
+from collections.abc import Callable
+from typing import Any
 
-listing_created = django.dispatch.Signal()
+class CustomSignals(Signal):
+    @classmethod
+    def mock(cls):
+        cls.send = Mock()
+        cls.send_robust = Mock()
+
+    @classmethod
+    def unmock(cls):
+        cls.send = super().send
+        cls.send_robust = super().send_robust
+
+
+
+listing_created = CustomSignals()
